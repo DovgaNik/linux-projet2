8ddc7049a54302b1d0be888ebf49a807acf9c801
DovgaNik <nikita.dovgan.05@hotmail.com>
2024-10-26 13:27:33 +0200

    Moved the display to a separate function

--- src/main.sh
+++ src/main.sh
@@ -1,32 +1,25 @@
 #!/bin/bash
 
 get_files_arr () {
-
-	folder_path=$1
-	find "$folder_path" -type f
+    folder_path=$1
+    find "$folder_path" -type f
 }
 
+display_all () {
+    local filenames=("$@") 
+    counter=0
+    echo "Displaying all the files available in /var/log/"
+    for filename in "${filenames[@]}"; do
+        echo "$counter: $filename" 
+        ((counter++))
+    done
+}
 
-
-
-
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-  exit
+if [ "$EUID" -ne 0 ]; then
+    echo "Please run as root"
+    exit
 fi
 
-
-
-
-
-
 filenames=($(get_files_arr /var/log))
-counter=0
 
-echo "Displaying all the files available in /var/log/"
-for filename in "${filenames[@]}"; 
-do
-
-	echo "$counter" "$filename"
-	let counter=counter+1	
-done
+display_all "${filenames[@]}"
