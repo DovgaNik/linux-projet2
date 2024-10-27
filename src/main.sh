bec90a225a9e088f86b592572baf589ef8d7a8f0
DovgaNik <nikita.dovgan.05@hotmail.com>
2024-10-27 11:51:11 +0100

    Added deletion of lines

--- src/main.sh
+++ src/main.sh
@@ -15,6 +15,32 @@ display_all () {
     done
 }
 
+delete_log_data() {
+    read -p "Select the file you would like to delete the lines from: " id_log_file 
+    selected_log_file=${filenames[$id_log_file]}
+    read -p "Enter the text or pattern to search for: " pattern
+
+    matches=$(grep -n "$pattern" "$selected_log_file")
+    if [[ -z "$matches" ]]; then
+        echo "No matches found."
+        return
+    fi
+
+    echo "The following lines match your search pattern:"
+    echo "$matches"
+
+    echo "Do you want to delete these lines? (y/n)"
+    read confirm
+    if [[ "$confirm" == "y" ]]; then
+        sed -i "/$pattern/d" "$selected_log_file"
+        echo "Lines containing '$pattern' have been deleted."
+    else
+        echo "Deletion canceled."
+    fi
+}
+
+
+
 if [ "$EUID" -ne 0 ]; then
     echo "Please run as root"
     exit
@@ -45,9 +71,36 @@ case $choice in
 	1)
 		display_all "${filenames[@]}"
 		;;
-	
+
 	2)
 		read -p "Please select the file id to be deleted. HINT: You can look the IDs up using the first option: " id_to_delete 
-		rm ${filenames[$id_to_delete]}	
-	;;
+	
+		echo "Do you want to delete that file? (y/n)"
+    		read confirm
+    		if [[ "$confirm" == "y" ]]; then
+        		echo "${filenames[$id_to_delete]} have been deleted."
+			rm ${filenames[$id_to_delete]}	
+
+    		else
+        		echo "Deletion canceled."
+    		fi
+
+
+		;;
+
+	3)
+		read -p "Please select the file id to be searched. HINT: You can look the IDs up using the first option: " id_to_select 
+		read -p "Please specify what you want to search: " search_regex
+
+		less +/${search_regex} ${filenames[$id_to_select]}
+		;;
+	4)
+		delete_log_data
+		;;
+
+	5)
+		read -p "Select the file you would like to read: " id_to_read
+		less ${filenames[$id_to_read]}
+		;;
+
 esac 
